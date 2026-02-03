3aefb3769f4d642f473bc4303b3236e58bc8c65e
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2026-02-03 10:10:48 +0530

    Added models

---
+++ models/Playlist.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const playlistSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    songs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Song'
+    }]
+})
