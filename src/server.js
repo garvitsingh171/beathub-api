9de8055744b9c2f63942ace13c2b36150700f1fb
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2026-02-18 15:20:44 +0530

    Modified directory to work with test cases

--- src/server.js
+++ src/server.js
@@ -1,26 +1,8 @@
-const express = require("express");
+const app = require("./app");
 require("dotenv").config();
 const { connectToDB } = require("../db/db");
 
-const app = express();
-
-app.use(express.json());
-
-app.use("/api/users", require("../src/routes/user.route"));
-
-app.use("/api/songs", require("../src/routes/song.route"));
-
-app.use("/api/playlist", require("../src/routes/playlist.route"));
-
-app.use('/api/artist', require('../src/routes/artist.route'));
-
-app.use('/api/album', require("../src/routes/album.route"));
-
 const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
-});
 
 async function start() {
     try {
