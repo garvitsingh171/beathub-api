fe0244388b0567d1eda4bec7cc2388ba01d8eee9
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2026-04-02 10:24:43 +0530

    jwt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema(
     {
@@ -15,6 +16,7 @@ const UserSchema = new mongoose.Schema(
         password: {
             type: String,
             required: true,
+            select: false,
         },
         likedSongs: [
             {
