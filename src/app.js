015990ab2e735c8df92839d466885e01d5de8294
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2026-04-28 14:45:04 +0530

    src/app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,15 @@ const express = require("express");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./config/swagger");
 const { notFoundHandler, globalErrorHandler } = require("./middlewares/error.middleware");
+const apiRateLimiter = require("./middlewares/rateLimit.middleware");
 
 const app = express();
 
+app.set("trust proxy", 1);
 app.use(express.json());
+app.use("/api", apiRateLimiter);
 
+app.use("/api/auth", require("../src/routes/auth.route"));
 app.use("/api/users", require("../src/routes/user.route"));
 app.use("/api/songs", require("../src/routes/song.route"));
 app.use("/api/playlist", require("../src/routes/playlist.route"));
